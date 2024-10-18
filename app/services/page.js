7ecf753bfcb14b70337607c652e47a2846a76296
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-18 11:08:24 +0100

    Added mp4 background

--- app/services/page.js
+++ app/services/page.js
@@ -3,11 +3,20 @@ import styles from '@/styles/Home.module.css'; // Import the CSS module
 
 export default function ServicesPage() {
   return (
+<div className="background-container">
+      {/* Video Background */}
+      <video autoPlay muted loop playsInline className="background-video">
+        <source src="/indexing.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+
     <div className={styles.homeContainer}>
       <h1 className={styles.gradientText}>Handy Applications</h1>
       <p className={styles.description}>
       Links To Other Applications Goes Here.
       </p>
     </div>
+  </div>
   );
 }
