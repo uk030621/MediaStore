b249a2bbe67c411437e70563301e46dfb5656e37
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-21 11:55:02 +0100

    Undo search/page.js

--- app/youtube/page.js
+++ app/youtube/page.js
@@ -5,7 +5,7 @@ import YouTubeSearch from "../components/YouTubeSearch";
 export default function HomePage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">YouTube Video Search</h1>
+      <h1 className="text-2xl font-bold mb-4 mt-4">YouTube Video Search</h1>
       <YouTubeSearch />
     </div>
   );
