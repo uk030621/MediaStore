8434e283aab3a291bae4c9d365e842391bc93ae7
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-21 10:07:35 +0100

    Added custom icon

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,18 @@ import Link from 'next/link';
 
 
 export const metadata = {
-  title: 'My Next.js App',
-  description: 'Responsive Next.js App with Header and Footer',
-}
+  title: 'Media Library App',
+  description: 'Developed by LWJ',
+  icons: {
+    icon: "/icons/icon-512x512.png", // Favicon
+    apple: "/icons/icon-180x180.png", // Apple touch icon for iOS home screen
+  },
+  manifest: "/manifest.json", // Link to your Web App Manifest
+};
+
+export const viewport = {
+  themeColor: "#000000", // Set theme color for browsers and devices here
+};
 
 export default function RootLayout({ children }) {
   return (
